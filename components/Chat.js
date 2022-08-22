d74bbf20d75ad3a0406538b72c6535b761a00b0a
shayne-wenzel <shayne.wenzel22@gmail.com>
2022-08-22 14:45:06 +0200

    First Commit

---
+++ components/Chat.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Chat(props) {
+    let { name, color } = props.route.params;
+
+    useEffect(() => {
+        props.navigation.setOptions({ title: name });
+    }, [])
+
+
+    return (
+        <View style={[{ backgroundColor: color }, styles.container]}>
+            <Text style={styles.text}>Hello Chat!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    text: {
+        color: '#ffffff'
+    },
+});
